ba26e852bc62599dd7a998aee3861acea12f29eb
nagendran <nagendran.ms@tthconsulting.com>
2020-11-10 18:11:21 +0530

    libCreated

---
+++ Util/src/main/java/com/tth/util/InternetConnection.java
@@ -0,0 +1,26 @@
+package com.tth.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetConnection {
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (NetworkInfo anInfo : info) {
+                    if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
