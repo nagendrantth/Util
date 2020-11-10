271f42a9ae7a981df603dea2360da6f261f5783f
nagendranms <nagendran.tth@gmail.com>
2020-11-10 18:44:24 +0530

    libCreated

--- app/src/main/java/com/tth/sampleutil/MainActivity.java
+++ app/src/main/java/com/tth/sampleutil/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.tth.util.InternetConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(new InternetConnection().isNetworkAvailable(getApplicationContext())){
+
+        }
     }
 }
